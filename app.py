e59f62b9573eb7f7603ed9244d5f10861c791f9b
Ecardenas107 <elcardenas@csumb.edu>
2026-03-17 10:51:17 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import qrcode
+from io import BytesIO
+
+st.title("QR Code Generator")
+
+data = st.text_input("Enter text or URL", "Python is fun.")
+img = qrcode.make(data)
+img.save("MyQRCode1.jpg")
+img = Image.open("MyQRCode1.jpg")
+st.image(img)
